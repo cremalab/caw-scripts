764b2c819e3293ab125b0f1fe57b96c8a20b202b
Rob LaFeve <roblafeve@gmail.com>
2019-11-27 22:53:59 -0600

    CAWS: Fix dist snapshot

--- __tests__/dist.ts
+++ __tests__/dist.ts
@@ -1,12 +1,16 @@
 import spawn = require("cross-spawn")
 
-const trimEnds = (a: string) => a.slice(1, -1)
+const removeFirstLine = (a: string) =>
+  a
+    .split("\n")
+    .slice(2)
+    .join("\n")
 
 describe("dist", () => {
   it("works", () => {
     spawn.sync("npm", ["run", "build"])
     process.chdir("./dist")
     const tree = spawn.sync("treee", ["-a", "-l 6"]).output.toString()
-    expect(trimEnds(tree)).toMatchSnapshot()
+    expect(removeFirstLine(tree)).toMatchSnapshot()
   })
 })
