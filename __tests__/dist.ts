d51e0a75d57ebe7eca0db12dfa83b5409eedcbbb
Rob LaFeve <roblafeve@gmail.com>
2019-11-27 22:37:31 -0600

    CAWS: Add various scripts and initial tests

---
+++ __tests__/dist.ts
@@ -0,0 +1,12 @@
+import spawn = require("cross-spawn")
+
+const trimEnds = (a: string) => a.slice(1, -1)
+
+describe("dist", () => {
+  it("works", () => {
+    spawn.sync("npm", ["run", "build"])
+    process.chdir("./dist")
+    const tree = spawn.sync("treee", ["-a", "-l 6"]).output.toString()
+    expect(trimEnds(tree)).toMatchSnapshot()
+  })
+})
